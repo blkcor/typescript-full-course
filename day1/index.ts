737ca079194e02490083e9f76d4bf0b92b961f80
chenzllong <1029129867@qq.com>
2022-09-05 13:14:24 +0800

    test

--- day1/index.ts
+++ day1/index.ts
@@ -1,1 +1,13 @@
+interface Blkcor {
+    name:string,
+    age:number,
+    mood:string,
+    good?:boolean
+}
 const a :string = 'sddd'
+
+class czl implements Blkcor{
+    name:'czl'
+    age: 12
+    mood: 'good'
+}
